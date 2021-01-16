e2bc04a24f4e081d62dd759630df3804c7e3faaa
shineprincess <shineprincess456@gmail.com>
2021-01-16 11:55:50 -0800

    fibonacci series

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void) {
+  int fib(int);
+  int num , i ;
+  printf("Enter a Number up to which you need Fibonacci series ");
+  scanf("%d", &num);
+
+  for(i=1 ; i<=num ; i++)
+    printf("%d ",fib(i));
+  return 0;
+}
+
+int fib(int n){
+  if(n==1 || n==2)
+    return(1);
+  return(fib(n-1)+fib(n-2));
+}
